d375b63b32d46d58c7154addcf7a4f5f85a44030
Maged Saeed <mageedsaeed1@gmail.com>
2022-10-01 14:06:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from .zipf import *
+def main():
+    # call zipf for natural language sample(s)
+    zipf(text1, "Moby Dick")         
+    zipf(text2, "Sense and Sensibility")        
+    zipf(text3,"The Book of Genesis")        
+    zipf(text4, "Inaugural Address Corpus")         
+    zipf(text5, "Sample Chat Corpus")         
+    # call zipf for random language sample(s)
+    generate_text(approx_word_len=6)
+    
+if __name__ == "__main__":
+    main()
